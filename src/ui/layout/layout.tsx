f1f5dcf07098d32c45b3ad87949c5d3044fc855f
alexndrkovalev <yakovalev1@mail.ru>
2024-09-13 23:11:28 +0300

    init: start layout

--- src/ui/layout/layout.tsx
+++ src/ui/layout/layout.tsx
@@ -1,8 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
 export const Layout = () => {
   return (
     <div>
       <header>header</header>
-      <main>{/*<Outlet />*/}</main>
+      <main>
+        <Outlet />
+      </main>
     </div>
   )
 }
