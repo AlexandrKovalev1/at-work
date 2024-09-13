192fae1be9525712c6eaf196060b926f8ac1bcab
alexndrkovalev <yakovalev1@mail.ru>
2024-09-13 12:30:41 +0300

    feat: add imports fonts

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App.tsx'
+import '@fontsource/manrope/400.css'
+import '@fontsource/manrope/500.css'
+import '@fontsource/manrope/600.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
