6a576f9eb23312f6e06556b4112141ac24f9efe2
alexndrkovalev <yakovalev1@mail.ru>
2024-09-13 12:43:29 +0300

    build:connected styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './App.tsx'
 import '@fontsource/manrope/400.css'
 import '@fontsource/manrope/500.css'
 import '@fontsource/manrope/600.css'
+import './styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
