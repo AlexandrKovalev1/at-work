b7508cb0c9f4e66007744715577a9c9bd53cb2b5
alexndrkovalev <yakovalev1@mail.ru>
2024-09-14 17:02:30 +0300

    feat: add user navigation component

---
+++ src/pages/edit-user/edit-user-navigation/edit-user-navigation.tsx
@@ -0,0 +1,32 @@
+import s from './edit-user-navigation.module.scss'
+import { Category, Typography } from '../../../components'
+import { PATH } from '../../../app/providers/router/router.tsx'
+
+export const EditUserNavigation = () => {
+  return (
+    <nav className={s.nav}>
+      <ul className={s.list}>
+        <li>
+          <Typography as={Category} to={PATH.USER_EDIT_PROFILE} variant={'text2-semibold'}>
+            Данные профиля
+          </Typography>
+        </li>
+        <li>
+          <Typography as={Category} to={'/'} variant={'text2-semibold'}>
+            Рабочее пространство
+          </Typography>
+        </li>
+        <li>
+          <Typography as={Category} to={'/'} variant={'text2-semibold'}>
+            Приватность
+          </Typography>
+        </li>
+        <li>
+          <Typography as={Category} to={'/'} variant={'text2-semibold'}>
+            Безопасность
+          </Typography>
+        </li>
+      </ul>
+    </nav>
+  )
+}
