3136517b7467e3b15999ee093f85ef6641a4f9d1
alexndrkovalev <yakovalev1@mail.ru>
2024-09-15 01:32:01 +0300

    feat:create dialog component

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,3 +1,43 @@
-export const Dialog = (props: Props) => {
-  return <div></div>
+import * as DialogRadix from '@radix-ui/react-dialog'
+import s from './dialog.module.scss'
+import { CrossIcon } from '../../assets/icons/CrossIcon.tsx'
+import { useEffect, useState } from 'react'
+import { CheckedBox } from '../../assets/icons/CheckedBox.tsx'
+import { Typography } from '../typography'
+import { useAppSelector } from '../../app/providers/store/store.ts'
+import { selectStatus } from '../../slices/app/app-slice.ts'
+
+export const Dialog = () => {
+  const status = useAppSelector(selectStatus)
+  const [open, setOpen] = useState(true)
+
+  useEffect(() => {
+    if (status === 'succeeded') {
+      if (!open) setTimeout(() => setOpen(false), 4000)
+      setOpen(!open)
+    }
+  }, [status, setOpen])
+
+  return (
+    <DialogRadix.Root open={open}>
+      <DialogRadix.Portal>
+        <DialogRadix.Overlay className={s.dialogOverlay} />
+        <DialogRadix.Content
+          aria-describedby={'some'}
+          className={s.dialogContent}
+          onPointerDownOutside={e => {
+            e.preventDefault()
+          }}
+        >
+          <DialogRadix.DialogDescription className={s.hidden} />
+          <DialogRadix.DialogTitle className={s.hidden} />
+          <button className={s.crossButton} onClick={() => setOpen(false)}>
+            <CrossIcon />
+          </button>
+          <CheckedBox />
+          <Typography variant={'headline'}>Изменения сохранены!</Typography>
+        </DialogRadix.Content>
+      </DialogRadix.Portal>
+    </DialogRadix.Root>
+  )
 }
