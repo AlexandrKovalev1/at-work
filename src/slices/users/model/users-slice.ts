dfb133e45f01b3f7642fae61e7cdf2361762e9d0
alexndrkovalev <yakovalev1@mail.ru>
2024-09-14 15:03:20 +0300

    feat: add editStatusUser and createSelector

---
+++ src/slices/users/model/users-slice.ts
@@ -0,0 +1,54 @@
+import { createAppSlice } from '../../../common/utils/createAppSlice.ts'
+import { CustomUser, DomainUser, UserStatus } from '../user.types.ts'
+import { userApi } from '../api/user-api.ts'
+import { isAxiosError } from 'axios'
+import { createSelector, PayloadAction } from '@reduxjs/toolkit'
+
+const slice = createAppSlice({
+  name: 'users',
+  initialState: {
+    users: [] as CustomUser[],
+  },
+  reducers: create => ({
+    fetchUsers: create.asyncThunk<DomainUser[]>(
+      async (_, { rejectWithValue }) => {
+        try {
+          const res = await userApi.fetchUsers()
+          return res.data.filter((_, index) => index <= 5)
+        } catch (e) {
+          if (isAxiosError(e)) {
+            return rejectWithValue({ message: e.message })
+          }
+          return rejectWithValue({ message: e })
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state.users = action.payload.map(u => ({ ...u, status: 'active' }))
+        },
+      }
+    ),
+    editStatusUser: create.reducer(
+      (state, action: PayloadAction<{ id: number; status: UserStatus }>) => {
+        state.users = state.users?.map(u =>
+          u.id === action.payload.id ? { ...u, status: action.payload.status } : u
+        )
+      }
+    ),
+  }),
+  selectors: {
+    selectActiveUsers: createSelector(
+      state => state.users,
+      (users: CustomUser[]) => users?.filter(user => user.status === 'active')
+    ),
+    selectArchivedUsers: createSelector(
+      state => state.users,
+      (users: CustomUser[]) => users?.filter(user => user.status === 'archive')
+    ),
+    selectAllUsers: state => state.users,
+  },
+})
+
+export const userReducer = slice.reducer
+export const usersThunks = slice.actions
+export const { selectAllUsers, selectActiveUsers, selectArchivedUsers } = slice.selectors
