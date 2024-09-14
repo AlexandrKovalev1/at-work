a1b9eef192afaf069b076267f09c047e2afc9236
alexndrkovalev <yakovalev1@mail.ru>
2024-09-14 23:01:38 +0300

    refactor: add app reducer

--- src/app/providers/store/store.ts
+++ src/app/providers/store/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { userReducer } from '../../../slices/users/model/users-slice.ts'
+import { appReducer } from '../../../slices/app/app-slice.ts'
 
 export const store = configureStore({
-  reducer: { users: userReducer },
+  reducer: { users: userReducer, app: appReducer },
 })
 
 export type AppDispatch = typeof store.dispatch
