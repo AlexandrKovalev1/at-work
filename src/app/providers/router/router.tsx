a23165aaba5672297b588fa5d3a18d5142a7b7f3
alexndrkovalev <yakovalev1@mail.ru>
2024-09-14 17:23:02 +0300

    refactor: add routes

--- src/app/providers/router/router.tsx
+++ src/app/providers/router/router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom'
 import { Layout } from '../../../ui/layout/layout.tsx'
 import { Users } from '../../../pages/users'
+import { EditUser } from '../../../pages/edit-user'
+import { EditProfile } from '../../../pages/edit-user/edit-profile'
+import { EditWorkspace } from '../../../pages/edit-user/edit_workspace'
 
 export const PATH = {
   ROOT: '/',
   USERS: '/users',
   USER_EDIT: 'edit_user',
   USER_EDIT_PAGE: 'edit_user/:id',
+  USER_EDIT_PROFILE: 'profile',
+  USER_EDIT_WORKSPACE: 'workspace',
 } as const
 
 const router = createBrowserRouter([
@@ -28,7 +33,15 @@ const router = createBrowserRouter([
           },
           {
             path: PATH.USER_EDIT_PAGE,
-            element: <div>Edit</div>,
+            element: <EditUser />,
+            children: [
+              {
+                index: true,
+                element: <Navigate to={PATH.USER_EDIT_PROFILE} />,
+              },
+              { path: PATH.USER_EDIT_PROFILE, element: <EditProfile /> },
+              { path: PATH.USER_EDIT_WORKSPACE, element: <EditWorkspace /> },
+            ],
           },
         ],
       },
